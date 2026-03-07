d77f056bcf91a37b2b9605d49c43cabd22e0b279
jakubkonstacky <jkonstacky@gmail.com>
2026-03-07 20:00:01 +0100

    Update next.config.js 6

--- next.config.js
+++ next.config.js
@@ -1,9 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  typescript: {
-    // Toto zajistí, že drobné chyby v kódu nezablokují spuštění webu
-    ignoreBuildErrors: true,
-  },
+  typescript: { ignoreBuildErrors: true },
+  eslint: { ignoreDuringBuilds: true },
+  output: 'standalone'
 }
-
 module.exports = nextConfig

b1a0ff0db0814cff75849cb052d25e34f0e77fae
jakubkonstacky <jkonstacky@gmail.com>
2026-03-07 19:48:42 +0100

    Update page.tsx 3

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import { createClient } from '@supabase/supabase-js'
 // Vyměň tyto dvě hodnoty za své ze Supabase
 const supabase = createClient(
   'https://yjpevqwklfehterabrwo.supabase.co', 
-  'TVŮJ_ANON_PUBLIC_KEY' 
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InlqcGV2cXdrbGZlaHRlcmFicndvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzI5MDI2MTgsImV4cCI6MjA4ODQ3ODYxOH0.NhqsP1tlia2qj_vEKrLDfpx-wZ72NeHw8uNAAM26kig' 
 )
 
 export default async function Page() {

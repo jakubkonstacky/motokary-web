10637cbec02e46cbb580b8920f242a2d436384db
jakubkonstacky <jkonstacky@gmail.com>
2026-03-07 20:39:31 +0100

    Update next.config.js 111

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  typescript: { ignoreBuildErrors: true },
-  eslint: { ignoreDuringBuilds: true },
-  output: 'standalone'
+  typescript: {
+    ignoreBuildErrors: true
+  },
 }
 module.exports = nextConfig

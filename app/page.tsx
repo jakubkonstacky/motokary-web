fdb6fcc8b78afa4eb38c83d91cbb705ed3568730
jakubkonstacky <jkonstacky@gmail.com>
2026-03-08 16:39:48 +0100

    Add import for Link component in page.tsx aaa

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,9 @@
 import { createClient } from '@supabase/supabase-js';
 import Link from 'next/link';
 
+
 const supabase = createClient(process.env.NEXT_PUBLIC_SUPABASE_URL!, process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!);
+
 
 export default async function HomePage() {
   const currentYear = new Date().getFullYear();

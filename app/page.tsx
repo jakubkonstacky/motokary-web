8a98af1cd353fa9c21879920ba60291dade013a5
jakubkonstacky <jkonstacky@gmail.com>
2026-03-09 01:26:37 +0100

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -35,12 +35,12 @@ export default async function HomePage() {
                   const dateStr = rDate.toLocaleDateString('cs-CZ', { day: '2-digit', month: '2-digit', year: 'numeric' });
                   return (
                     <tr key={race.id} style={{ borderBottom: idx === (races.length - 1) ? 'none' : '1px solid rgba(255,255,255,0.05)' }}>
-                      <td style={{ ...THEME.td, padding: '15px 20px', width: '240px', color: '#fbbf24', fontWeight: '800', whiteSpace: 'nowrap' }}>
+                      <td style={{ ...THEME.td, padding: '15px 20px', width: '140px', color: '#fbbf24', fontWeight: '800', whiteSpace: 'nowrap' }}>
                         {dateStr}
-                      </td>                            
-                      <td style={{ ...THEME.td, padding: '15px 20px',width: '340px', textAlign: 'left' fontWeight: '700' }}>
-                      {isPast ? (
-                          <Link href={`/detail_vysledky?id=${race.id}`} style={{ color: '#fff', textDecoration: 'none' }}> {race.name} → Zobraz výsledky</Link>
+                      </td>
+                      <td style={{ ...THEME.td, padding: '15px 20px', fontWeight: '700' }}>
+                        {isPast ? (
+                          <Link href={`/detail_vysledky?id=${race.id}`} style={{ color: '#fff', textDecoration: 'none' }}>{race.name} →</Link>
                         ) : race.name}
                       </td>
                     </tr>
